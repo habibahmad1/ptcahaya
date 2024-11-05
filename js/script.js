97f8fd69f50d6d5b0adcb0ee557f63b03c63246d
Habib Ahmad <habibahmad2597@gmail.com>
2024-11-05 13:53:39 +0700

    add product

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
+let lastScrollTop = 0;
+const navbar = document.querySelector(".navbar");
 
+window.addEventListener("scroll", function () {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (scrollTop > lastScrollTop) {
+    // Scroll ke bawah
+    navbar.classList.add("hidden");
+  } else {
+    // Scroll ke atas
+    navbar.classList.remove("hidden");
+  }
+
+  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Untuk menghindari angka negatif
+});
