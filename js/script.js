29b4cbc41cbe543e544404146fff5f3693770f6a
Habib Ahmad <habibahmad2597@gmail.com>
2024-11-07 13:46:24 +0700

    add scroll up

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
 let lastScrollTop = 0;
 const navbar = document.querySelector(".navbar");
+const up = document.querySelector(".button-up");
 
 window.addEventListener("scroll", function () {
   let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  const documentHeight = document.documentElement.scrollHeight; // Tinggi dokumen
+  const threshold = documentHeight * 0.7; // 80% dari tinggi dokumen
 
+  // Logika untuk navbar
   if (scrollTop > lastScrollTop) {
     // Scroll ke bawah
     navbar.classList.add("hidden");
   } else {
     // Scroll ke atas
     navbar.classList.remove("hidden");
+  }
+
+  // Logika untuk tombol scroll to top
+  if (scrollTop > threshold) {
+    up.classList.add("show"); // Tampilkan tombol
+  } else {
+    up.classList.remove("show"); // Sembunyikan tombol
   }
 
   lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Untuk menghindari angka negatif
@@ -21,3 +32,8 @@ const navbarhp = document.querySelector(".navbar-side");
 button.addEventListener("click", () => {
   navbarhp.classList.toggle("active"); // Toggle kelas "active"
 });
+
+// Menangani klik pada tombol scroll to top
+up.onclick = function () {
+  window.scrollTo({ top: 0, behavior: "smooth" }); // Scroll ke atas dengan halus
+};
